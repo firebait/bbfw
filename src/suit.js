e9121afc97c006214858eb3b6a155da34d58b2b1
marco <marco@set.tv>
2014-11-05 16:40:17 -0800

    Initialize application automatically

--- src/suit.js
+++ src/suit.js
@@ -3,3 +3,16 @@ Suit = {
     /** @namespace */
     Components: {}
 };
+
+// Start application.
+$( function () {
+    // Prepare the body.
+    $('body').prepend(App.mainView.render().el);
+
+    // Start routes.
+    App.mainRouter = new App.Routers.Main();
+    _.each(App.Routers, function (value, key) {
+        new App.Routers[key]();
+    });
+    Backbone.history.start({pushState: false});
+});
